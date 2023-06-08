0c64f54d03eef5ce339e47e5b69463b6a712ead7
BlackhatL <lpiderit@gmail.com>
2023-06-08 01:47:25 -0400

    subiendo la base del proyecto

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AlumnosController;
+use App\Http\Controllers\ProfesoresController;
+use App\Http\Controllers\AdministradorController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/alumno',[AlumnosController::class,'alumno'])->name('alumno.index');
+
+Route::get('/profesor',[ProfesoresController::class,'profesor'])->name('profesor.index');
+
+Route::get('/administrador',[AdministradorController::class,'administrador'])->name('administrador.index');
