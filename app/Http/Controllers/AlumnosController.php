d15cbadb3fb1bcde640c3ca24e9899b6291e4e62
BlackhatL <lpiderit@gmail.com>
2023-06-09 04:29:56 -0400

    arreglo en el controlador de alumno

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -8,6 +8,7 @@ use App\Models\estudiantes;
 class AlumnosController extends Controller
 {
     public function index(){
+        $estudiantes = estudiantes::all();
         return view('alumno.index',compact('estudiantes'));
     }
 
