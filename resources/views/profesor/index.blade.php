fb7c323bd0a1f3386a0a26db3ef6aee29ab57a20
Lolregsito <amigocordovanelson@gmail.com>
2023-06-09 05:31:38 -0400

    zona de comentario

--- resources/views/profesor/index.blade.php
+++ resources/views/profesor/index.blade.php
@@ -17,15 +17,19 @@
         <span class="navbar-brand mb-0 h1">Bienvenido</span>
     </div>
 </nav>
-<body>
+<body class="bg-warning">
     <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-primary">Aprobar</button>
-        <button type="button" class="btn btn-danger">Rechazar</button>
-        <button type="button" class="btn btn-primary">Modificar</button>
+        <button type="button" class="btn btn-outline-success">Aprobar</button>
+        <button type="button" class="btn btn-outline-danger">Rechazar</button>
+        <button type="button" class="btn btn-outline-dark">Modificar</button>
     </div>
-    <div>
-        <button type="button" class="btn btn-info">Agregar Comentario</button>
-    </div>
+    <form>
+        <div class="col-6 mb-3">
+            <label for="zona_de_comentario" class="form-label">Ingrese un comentario a la propuesta</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <button type="submit" class="m-3 p-0 btn btn-outline-info">Agregar Comentario</button>
+          </div>
+    </form>
 
 
 </body>
