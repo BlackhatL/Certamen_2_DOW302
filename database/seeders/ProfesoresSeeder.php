7d117622d90178c7de3afbe5a052e326544b291b
BlackhatL <lpiderit@gmail.com>
2023-06-09 02:40:10 -0400

    estructurando seeder

--- database/seeders/ProfesoresSeeder.php
+++ database/seeders/ProfesoresSeeder.php
@@ -4,11 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ProfesoresSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * @return void
      */
     public function run(): void
     {
