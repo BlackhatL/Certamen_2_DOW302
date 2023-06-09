f2c503e68725831e5c059f80a7aae6815fc92485
Lolregsito <amigocordovanelson@gmail.com>
2023-06-09 04:20:57 -0400

    Empezando con la vista de profesor

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,7 +10,7 @@
     <title>Equipos</title>
 </head>
 
-<body class="bg-light">
+<body class="bg-secondary">
     <h1 class="border container-fluid text-center text-white">Página Principal</h1>
     <div class="container text-center">
         <div class="row h-100">
@@ -19,11 +19,11 @@
             <div class="card-group position-absolute top-50 end-0 translate-middle-y">
                 <div class="col-sm-4 mb-3 mb-sm-0">
 
-                    <div class="card text-bg-primary h-100">
+                    <div class="card text-bg-success h-100">
                         <div class="card-body">
                             <h4 class="card-title">Alumno</h4>
                             <p class="card-text">Vista del Alunmo, donde se puede subir y eliminar archivos PDF</p>
-                            <a href="{{route('alumno.index')}}" class="btn btn-secondary">Ingresar</a>
+                            <a href="{{route('alumno.index')}}" class="btn btn-light">Ingresar</a>
                         </div>
                     </div>
 
@@ -31,11 +31,11 @@
                 <!--CARD PROFESOR-->
                 <div class="col-sm-4 mb-3 mb-sm-0">
 
-                    <div class="card text-bg-primary h-100">
+                    <div class="card text-bg-info h-100">
                         <div class="card-body">
                             <h4 class="card-title">Profesor</h4>
                             <p class="card-text">Vista del Profesor, donde se puede revisar y comentar los archivos PDF de los alumnos</p>
-                            <a href="Alumno.html" class="btn btn-secondary">Ingresar</a>
+                            <a href="{{route('profesor.index')}}" class="btn btn-light">Ingresar</a>
                         </div>
                     </div>
 
@@ -43,11 +43,11 @@
                 <!--CARD ADMINISTRADOR-->
                 <div class="col-sm-4 mb-3 mb-sm-0">
 
-                    <div class="card text-bg-warning">
+                    <div class="card text-bg-danger">
                         <div class="card-body">
                             <h4 class="card-title">Administrador</h4>
                             <p class="card-text">Vista del Administrador, donde se puede gestionar profesores, estudiantes y el estado de las propuestas</p>
-                            <a href="Administrador.html" class="btn btn-secondary">Ingresar</a>
+                            <a href="Administrador.html" class="btn btn-light">Ingresar</a>
                         </div>
                     </div>
 
--- resources/views/profesor/index.blade.php
+++ resources/views/profesor/index.blade.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!--CSS-->
+    <link rel="stylesheet" href="/css/bootstrap-custom.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Profesores</title>
 </head>
+<nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Bienvenido</span>
+    </div>
+</nav>
 <body>
-    <h1>vista de profesor</h1>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">Aprobar</button>
+        <button type="button" class="btn btn-danger">Rechazar</button>
+        <button type="button" class="btn btn-primary">Modificar</button>
+    </div>
+    <div>
+        <button type="button" class="btn btn-info">Agregar Comentario</button>
+    </div>
+
+
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
